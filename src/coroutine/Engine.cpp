664edfd5ff64cd44fcbd4edb6c85841734fca8b4
twinkle <st.msu.cmc@gmail.com>
2019-12-21 11:52:05 +0300

    fix

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -8,8 +8,13 @@ namespace Coroutine {
 
 void Engine::Store(context &ctx) {
     char curr_pos;
-    ctx.Low = &curr_pos;
+    ctx.Low = StackBottom;
     ctx.High = StackBottom;
+    if (&curr_pos > ctx.Low) {
+        ctx.High = &curr_pos;
+    } else {
+        ctx.Low = &curr_pos;
+    }
     std::size_t size = ctx.High - ctx.Low;
     if (size > std::get<1>(ctx.Stack)) {
         delete std::get<0>(ctx.Stack);
@@ -42,14 +47,16 @@ void Engine::yield() {
 
 void Engine::sched(void *routine_) {
     context *ctx = static_cast<context *>(routine_);
-    if (ctx != nullptr) {
-        if (setjmp(cur_routine->Environment) > 0) {
-            return;
+    if (cur_routine != routine_) {
+        if (ctx != nullptr) {
+            if (setjmp(cur_routine->Environment) > 0) {
+                return;
+            }
+            Store(*cur_routine);
         }
-        Store(*cur_routine);
+        cur_routine = ctx;
+        Restore(*cur_routine);
     }
-    cur_routine = ctx;
-    Restore(*cur_routine);
 }
 
 } // namespace Coroutine
